25296c5c97a2fc452ff39aabc3316d06c5200f96
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-01-22 08:48:15 -0500

    Updated from Brython Server: 1/22/2018 8:48:15 AM

--- stringAnalysis.py
+++ stringAnalysis.py
@@ -1,1 +1,13 @@
+#Heather Stafford
+#1/22/18
+#stringAnalysis.py
+
+text = input('Enter a sentence: ')
+words = int(text.count(' '))
+char = int(len(text))
+print('Your sentence has',words,'words and',char,'characters and',(char - words),'letters')
+search = input('Enter a character to search for, make it upper case: ')
+phrase = text.upper()
+letter = phrase.count(search)
+print('your sentence has',letter,'of the character',search)
 
