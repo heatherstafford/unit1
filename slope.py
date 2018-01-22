924a5d11767c8f9999986cd3281d1ebe2771fe7e
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-01-22 08:31:05 -0500

    Updated from Brython Server: 1/22/2018 8:31:05 AM

--- slope.py
+++ slope.py
@@ -2,10 +2,10 @@
 #1/19/18
 #slope.py
 
-x1 = int(input('enter x1'))
-y1 = int(input('enter y1'))
-x2 = int(input('enter x2'))
-y2 = int(input('enter y2'))
+x1 = int(input('x1 ='))
+y1 = int(input('y1 ='))
+x2 = int(input('x2 ='))
+y2 = int(input('y2 ='))
 slope = (y2-y1)/(x2-x1)
 print('The slope is',slope)
 print('The equation of the line is Y = ',slope,'X + 2.0')
