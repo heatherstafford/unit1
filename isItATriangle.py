09b519a8d67d81fc1fe5da1ecc4c8c81fce08e7e
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-01-25 08:32:22 -0500

    Updated from Brython Server: 1/25/2018 8:32:21 AM

--- isItATriangle.py
+++ isItATriangle.py
@@ -1,1 +1,9 @@
+#Heather Stafford
+#1/25/18
+#isItATriangle.py
 
+sideA = int(input('Enter Side A: '))
+sideB = int(input('Enter Side B: '))
+sideB = int(input('Enter Side C: '))
+print(max(a,b,c))
+print(min(a,b,c))
