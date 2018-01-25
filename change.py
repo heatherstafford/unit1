fe3d5803624100d2683512f050322100530c742d
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-01-25 08:24:07 -0500

    Updated from Brython Server: 1/25/2018 8:24:07 AM

--- change.py
+++ change.py
@@ -2,4 +2,10 @@
 #1/25/18
 #change.py
 
-change = int(input
+change = int(input('Enter amount of cents: '))
+quarters = int(change/25)
+print('Quarters:', quarters)
+remainder1 = change%25 
+dimes = int(remainder1/10)
+print('Dimes: ',dimes)
+remainder2 = int(remainder1%5)
