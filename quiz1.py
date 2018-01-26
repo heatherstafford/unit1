4f821f0a6f4b1f8806d80bc14d1f05892dac709d
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-01-26 08:15:28 -0500

    Updated from Brython Server: 1/26/2018 8:15:24 AM

--- quiz1.py
+++ quiz1.py
@@ -1,1 +1,10 @@
+#Heather Stafford
+#1/26/18
+#quiz1.py
 
+print('Hello')
+name = input('What is your name? ')
+num1 = input('Enter your favorite number: ')
+print(name *num)
+num2 = input('Enter your second favorite number: ')
+print(num1**num2)
